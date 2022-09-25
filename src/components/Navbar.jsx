b5104d41f0d4d91a230322092389be901cfdcd08
Eisenhearth <95835712+Eisenhearth@users.noreply.github.com>
2022-09-25 10:13:57 +0800

    Errors Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
           <ul className="list-none flex flex-col justify-end items-center flex-1">
             {navLinks.map((nav, index) => (
               <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}> 
-              <a href={`#${nav.id}` onClick={handleClose}}>
+              <a href={`#${nav.id}`} onClick={handleClose}>
                 {nav.title}
               </a>
               </li>
